2de9e34782ba8c195273855d4ac1ea0333bef40a
Ben Bleything <ben@bleything.net>
2012-08-10 18:13:23 -0400

    only require stomp if using the stomp adapter

--- lib/klomp/client.rb
+++ lib/klomp/client.rb
@@ -1,6 +1,5 @@
 require 'onstomp'
 require 'onstomp/failover'
-require 'stomp'
 require 'json'
 require 'uuid'
 require 'logger'
@@ -66,6 +65,8 @@ module Klomp
           @read_conn = [@write_conn]
         end
       when :stomp
+        require 'stomp'
+
         # Failover in the Stomp library is kind of flaky. If the client
         # temporarily loses its connection, it is eventually able to reconnect
         # and resume sending messages. However, the subscribe thread never
