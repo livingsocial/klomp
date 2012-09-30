015d636aba602025c4bd905b212556dca862992a
Nick Sieger <nick@nicksieger.com>
2012-09-29 22:17:49 -0500

    Specs for CONNECT and CONNECTED frames

---
+++ spec/loldance/frames_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Loldance::Frames do
+
+  context "CONNECT" do
+
+    Given(:options) { {'login' => 'admin', 'password' => 'password', 'host' => '127.0.0.1'} }
+
+    When(:connect) { Loldance::Frames::Connect.new(options).to_s }
+
+    Then { connect.should == frame(:connect) }
+
+  end
+
+  context "CONNECTED" do
+
+    When(:connected) { Loldance::Frames::Connected.new frame(:connected) }
+
+    Then { connected.headers['version'].should == "1.1" }
+
+  end
+
+end
