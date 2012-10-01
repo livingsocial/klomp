87363f20fbe5d630b3dd708ccaab3d5bfdf7407d
Nick Sieger <nick@nicksieger.com>
2012-10-01 10:46:05 -0500

    Start of acceptance specs, connect + publish

---
+++ spec/acceptance/acceptance_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'json'
+require 'open-uri'
+
+describe "Loldance acceptance", :acceptance => true do
+
+  Given(:server) { "127.0.0.1:61613" }
+  Given(:credentials) { %w(admin password) }
+  Given(:options) { Hash[*%w(login passcode).zip(credentials).flatten] }
+  Given(:clients) { [] }
+  Given(:loldance) { Loldance.new(server, options).tap {|l| clients << l } }
+
+  context "connect" do
+
+    When { loldance }
+
+    Then { loldance.should be_connected }
+
+  end
+
+  context "publish" do
+
+    When { loldance.publish "/queue/greeting", "hello" }
+
+    Then do
+      vhosts = apollo_api_get("/broker/virtual-hosts.json")
+      @vhost = vhosts['rows'].detect {|row| row['queues'].include?('greeting') }['id']
+      queue = apollo_api_get("/broker/virtual-hosts/#{@vhost}/queues/greeting.json")
+      queue['metrics']['queue_items'].to_i.should > 0
+    end
+
+    after do
+      `curl -s -X DELETE -u #{credentials.join(':')} http://localhost:61680/broker/virtual-hosts/#{@vhost}/queues/greeting.json`
+    end
+  end
+
+  after { clients.each(&:disconnect) }
+
+  def apollo_api_get(path)
+    open("http://localhost:61680#{path}",
+         :http_basic_authentication => credentials) {|f| JSON::parse(f.read) }
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,10 +11,23 @@ module Frames
   end
 end
 
+def queue_available?
+  s = TCPSocket.new 'localhost', 61613
+  true
+rescue
+  false
+ensure
+  s && s.close
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+
+  unless queue_available?
+    config.filter_run_excluding :acceptance
+  end
 
   config.include Frames
 end
